986b2cdc0dc548352687ebf830ac46e7c7a289ba
BastianHaider <bastian.haider@gmx.at>
2019-01-17 14:30:52 -0800

    finished

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -14,11 +14,75 @@
  #include "deletable_primes.h"
  #include <math.h>
 
- unsigned long remove_digit(int index, unsigned long n)
+ unsigned long remove_digit(int index, unsigned long number)
  {
-   return 0;
+   unsigned long leftNumber = 0;
+   unsigned long rightNumber = 0;
+   unsigned long removedNumber = 0;
+
+   if (index == 0)
+   {
+     return number / 10;
+   }
+
+   leftNumber =number/ pow(10 , index + 1);
+   rightNumber = number % (unsigned long)pow(10 , index);
+   removedNumber =leftNumber * pow(10 , index) + rightNumber;
+   return removedNumber;
  }
- int get_ways(unsigned long p)
+ int get_ways(unsigned long number)
  {
-   return 0;
+    int length = 0;
+    for (int i = 0; i < get_length(number); i++)
+    {
+      if (get_length(number) == 1)
+      {
+        if (is_prime(number))
+        {
+          return 1;
+        }
+        return 0;
+      }
+      else if (is_prime(remove_digit(i,number)))
+      {
+        length += get_ways(remove_digit(i,number));
+       }
+     }
+    return length;
  }
+ bool is_prime(unsigned long number)
+ {
+    if (number == 1)
+    {
+      return false;
+    }
+    else if (number == 2)
+    {
+       return true;
+    }
+    else if ( number % 2 ==0)
+    {
+       return false;
+    }
+
+    unsigned long sqroot = sqrt(number);
+    for (unsigned long i = 3; i <= sqroot; i += 2 )
+    {
+       if (!(number % i))
+       {
+          return false;
+       }
+    }
+    return true;
+ }
+ int get_length(unsigned long number)
+{
+  int length = 0;
+  while (number > 0)
+  {
+     length++;
+     number /= 10;
+  }
+
+  return length;
+}
--- deletable_primes.h
+++ deletable_primes.h
@@ -2,7 +2,9 @@
 #ifndef ___DELETABLE_PRIMES
 #define ___DELETABLE_PRIMES
 
-unsigned long remove_digit(int index, unsigned long n);
-int get_ways(unsigned long p);
+unsigned long remove_digit(int index, unsigned long number);
+int get_ways(unsigned long number);
+bool is_prime(unsigned long number);
+int get_length(unsigned long number);
 
 #endif
